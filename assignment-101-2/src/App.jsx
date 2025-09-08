b3a84f59983c9c862ba0ccfd6f231206bca76b25
dev nayak10 <devprasannanayak@gmail.com>
2025-09-08 11:14:53 +0530

    Assignment-101-2: content added

---
+++ assignment-101-2/src/App.jsx
@@ -0,0 +1,11 @@
+import { FruitShop } from "./Fruit"
+
+function App() {
+  
+
+  return (
+    <FruitShop/>
+  )
+}
+
+export default App
---
+++ assignment-101-2/src/Fruit.jsx
@@ -0,0 +1,18 @@
+function Fruit(){
+    return(
+        <>
+            <h1>Apple</h1>
+            <h1>PineApple</h1>
+            <h1>Avocado</h1>
+            <h1>Mango</h1>
+        </>
+    );
+}
+
+function FruitShop(){
+    return(
+        Fruit()
+    );
+}
+
+export {FruitShop}
