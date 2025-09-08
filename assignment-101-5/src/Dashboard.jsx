e42f2fc7f904a58c8c09e59a4f29fd3489e9adb9
dev nayak10 <devprasannanayak@gmail.com>
2025-09-08 23:08:39 +0530

    content added

---
+++ assignment-101-5/src/Dashboard.jsx
@@ -0,0 +1,11 @@
+import Greeting from "./Greeting"
+function Dashboard(){
+    return(
+        <>
+            <Greeting/>
+            <h1>This is our Dashboard</h1>
+        </>
+    );
+}
+
+export default Dashboard;
---
+++ assignment-101-5/src/Greeting.jsx
@@ -0,0 +1,7 @@
+function Greeting(){
+    return(
+        <h1>Hello! welcome</h1>
+    );
+}
+
+export default Greeting;
